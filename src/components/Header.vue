40651aa8bd9f1d8d2783fec51360a746ff6231d1
Bae, Seong Ho <grulswizzi@gmail.com>
2019-11-25 17:16:32 +0900

    Add Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app-bar app clipped-left color="transparent" dense>
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+    <v-icon class="mx-4">fab fa-youtube</v-icon>
+    <v-toolbar-title class="mr-12 align-center">
+      <span class="title">3 Idiots</span>
+    </v-toolbar-title>
+    <v-spacer />
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "header"
+};
+</script>
+
+<style></style>
