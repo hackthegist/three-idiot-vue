c6269b5c85661f57f1fb6fc91cc5319ae9436391
Bae, Seong Ho <grulswizzi@gmail.com>
2019-11-26 11:28:59 +0900

    Update Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,12 +6,18 @@
       <span class="title">3 Idiots</span>
     </v-toolbar-title>
     <v-spacer />
+    <div>
+      <v-btn text color="white" to="/admin-movie-list">영화 관리</v-btn>
+      <v-btn text color="white" to="/admin-user-list">유저 관리</v-btn>
+    </div>
+    <v-btn text color="white" to="/movie-list">내 취향 분석</v-btn>
+    <v-btn text color="white" to="/movie-list">추천 영화 보기</v-btn>
   </v-app-bar>
 </template>
 
 <script>
 export default {
-  name: "header"
+  name: "Header"
 };
 </script>
 
